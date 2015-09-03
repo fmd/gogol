a30ccc3d1d919fcda8419205fd2de5f055d11294
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-03 23:07:55 +0100

    Rear window

--- window.go
+++ window.go
@@ -3,12 +3,13 @@ package bronson
 import (
     "github.com/go-gl/gl/v2.1/gl"
     "github.com/veandco/go-sdl2/sdl"
+    "fmt"
 )
 
 type Window struct {
     Title string
-    Width int
-    Height int
+    Width int32
+    Height int32
     Window *sdl.Window
     Context sdl.GLContext
 }
@@ -24,8 +25,8 @@ func NewWindow(opts WindowOpts) *Window {
 
     w := &Window{
         Title: opts.Title,
-        Width: opts.Width,
-        Height: opts.Height,
+        Width: int32(opts.Width),
+        Height: int32(opts.Height),
     }
 
     if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
@@ -39,7 +40,7 @@ func NewWindow(opts WindowOpts) *Window {
     w.Window, err = sdl.CreateWindow(w.Title,
                                      sdl.WINDOWPOS_UNDEFINED,
                                      sdl.WINDOWPOS_UNDEFINED,
-                                     w.Width, w.Height, sdl.WINDOW_OPENGL)
+                                     int(w.Width), int(w.Height), sdl.WINDOW_OPENGL)
     if err != nil {
         panic(err)
     }
@@ -49,13 +50,53 @@ func NewWindow(opts WindowOpts) *Window {
         panic(err)
     }
 
+    /*
     gl.Enable(gl.DEPTH_TEST)
     gl.ClearColor(0.2, 0.2, 0.3, 1.0)
     gl.ClearDepth(1)
     gl.DepthFunc(gl.LEQUAL)
     gl.Viewport(0, 0, int32(w.Width), int32(w.Height))
+    */
+
+    w.SetVideoOptions()
 
     return w
+}
+
+func (w *Window) SetVideoOptions() {
+    fmt.Println(fmt.Sprintf(" -Vendor: %s", gl.GetString(gl.VENDOR)))
+    fmt.Println(fmt.Sprintf(" -Renderer: %s\n", gl.GetString(gl.RENDERER)))
+    fmt.Println(fmt.Sprintf(" -Version: %s\n", gl.GetString(gl.VERSION)))
+    fmt.Println(fmt.Sprintf(" -Extensions: %s\n", gl.GetString(gl.EXTENSIONS)))
+
+    gl.ClearColor(0.0,0.0,0.0,1.0)
+
+    gl.MatrixMode(gl.PROJECTION)
+    gl.LoadIdentity()
+
+    gl.Viewport(0, 0, w.Width, w.Height)
+    gl.Ortho(-float64(w.Width)/2, float64(w.Width)/2, -float64(w.Height)/2, float64(w.Height)/2, -1.0, 1.0)
+
+    //Required for per-pixel placing.
+    gl.Translatef (0.375, 0.375, 0.0)
+
+    gl.MatrixMode(gl.MODELVIEW)
+    gl.LoadIdentity()
+
+    gl.Enable(gl.TEXTURE_2D)
+    gl.Disable(gl.LIGHTING)
+    gl.Disable(gl.DITHER)
+
+    gl.Enable(gl.CULL_FACE)
+    gl.CullFace(gl.FRONT)
+
+    gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+
+    gl.Enable(gl.BLEND)
+    gl.Enable(gl.ALPHA_TEST)
+    gl.DepthFunc(gl.LEQUAL)
+
+    gl.Disable(gl.DEPTH_TEST)
 }
 
 func (w *Window) Swap() {
