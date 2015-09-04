84dcac62739492606199bec98747efb57f631b3e
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-04 11:37:40 +0100

    Add doc

---
+++ bronson.go
@@ -0,0 +1,43 @@
+package bronson
+
+// The global Bronson object.
+var B *Bronson
+
+// The type structure of the global Bronson object.
+// There should only ever be one of these.
+type Bronson struct {
+    Quit bool
+    Window *Window
+    Input *Input
+}
+
+// Initialises Bronson.
+// This function is not called 'New' to signify it should only be called once.
+func Init(opts WindowOpts) *Bronson {
+    B = &Bronson{
+        Window: NewWindow(opts),
+        Input: NewInput(),
+    }
+
+    return B
+}
+
+
+func ShouldQuit() bool {
+    return B.Quit
+}
+
+// Processes one frame.
+// This should be called on each pass of the game's loop.
+func ProcessOneFrame() {
+    B.Input.Handle()
+    B.Window.Swap()
+    B.Window.Clear()
+    RenderTheFuckingSquare()
+}
+
+// Quits Bronson. Defer this to the end of your app's main function.
+// Destroys the window, cleans up the openGL context.
+func Cleanup() {
+    B.Window.Destroy()
+}
