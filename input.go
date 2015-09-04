e095dc64035c34b673cdbc71ce2dd76a779b24c8
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-04 11:37:48 +0100

    Refactor

--- input.go
+++ input.go
@@ -4,11 +4,54 @@ import (
     "github.com/veandco/go-sdl2/sdl"
 )
 
-type EventBuffer []sdl.Event
+// FIX KEY REPEAT
 
-func ProcessOneFrameOfInput(buffer *EventBuffer) {
+type KeyEventHandler []func(KeyCode)
+type KeyEventHandlerMap map[KeyCode][]func(KeyCode)
+
+type Input struct {
+    KeyDownHandlers KeyEventHandlerMap
+    KeyUpHandlers KeyEventHandlerMap
+}
+
+func NewInput() *Input {
+    return &Input{
+        KeyDownHandlers: make(KeyEventHandlerMap),
+        KeyUpHandlers: make(KeyEventHandlerMap),
+    }
+}
+
+func (i *Input) Handle() {
     var event sdl.Event
     for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-        *buffer = append(*buffer, event)
+        switch t := event.(type) {
+            case *sdl.QuitEvent:
+                B.Quit = true
+            case *sdl.MouseMotionEvent:
+                break
+                //fmt.Printf("[%d ms] MouseMotion\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n",
+                //           t.Timestamp, t.Which, t.X, t.Y, t.XRel, t.YRel)
+            case *sdl.KeyDownEvent:
+                code := sdl.GetScancodeFromKey(t.Keysym.Sym)
+                i.KeyDownHandlers.trigger(KeyCode(code))
+            case *sdl.KeyUpEvent:
+                code := sdl.GetScancodeFromKey(t.Keysym.Sym)
+                i.KeyUpHandlers.trigger(KeyCode(code))
+        }
+    }
+}
+
+func OnKeyDown(code KeyCode, fn func(KeyCode)) {
+    B.Input.KeyDownHandlers[code] = append(B.Input.KeyDownHandlers[code], fn)
+}
+
+func OnKeyUp(code KeyCode, fn func(KeyCode)) {
+    B.Input.KeyUpHandlers[code] = append(B.Input.KeyUpHandlers[code], fn)
+}
+
+func (m KeyEventHandlerMap) trigger(code KeyCode) {
+    h := m[code]
+    for _, fn := range h {
+        fn(code)
     }
 }
--- window.go
+++ window.go
@@ -3,16 +3,7 @@ package bronson
 import (
     "github.com/go-gl/gl/v2.1/gl"
     "github.com/veandco/go-sdl2/sdl"
-    "fmt"
 )
-
-type Window struct {
-    Title string
-    Width int32
-    Height int32
-    Window *sdl.Window
-    Context sdl.GLContext
-}
 
 type WindowOpts struct {
     Title string
@@ -20,83 +11,28 @@ type WindowOpts struct {
     Height int
 }
 
-func NewWindow(opts WindowOpts) *Window {
-    var err error
-
-    w := &Window{
-        Title: opts.Title,
-        Width: int32(opts.Width),
-        Height: int32(opts.Height),
-    }
-
-    if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-        panic(err)
-    }
-
-    if err = gl.Init(); err != nil {
-        panic(err)
-    }
-
-    w.Window, err = sdl.CreateWindow(w.Title,
-                                     sdl.WINDOWPOS_UNDEFINED,
-                                     sdl.WINDOWPOS_UNDEFINED,
-                                     int(w.Width), int(w.Height), sdl.WINDOW_OPENGL)
-    if err != nil {
-        panic(err)
-    }
-
-    w.Context, err = sdl.GL_CreateContext(w.Window)
-    if err != nil {
-        panic(err)
-    }
-
-    /*
-    gl.Enable(gl.DEPTH_TEST)
-    gl.ClearColor(0.2, 0.2, 0.3, 1.0)
-    gl.ClearDepth(1)
-    gl.DepthFunc(gl.LEQUAL)
-    gl.Viewport(0, 0, int32(w.Width), int32(w.Height))
-    */
-
-    w.SetVideoOptions()
-
-    return w
+type Window struct {
+    Title string
+    Width int
+    Height int
+    Window *sdl.Window
+    Context sdl.GLContext
 }
 
-func (w *Window) SetVideoOptions() {
-    fmt.Println(fmt.Sprintf(" -Vendor: %s", gl.GetString(gl.VENDOR)))
-    fmt.Println(fmt.Sprintf(" -Renderer: %s\n", gl.GetString(gl.RENDERER)))
-    fmt.Println(fmt.Sprintf(" -Version: %s\n", gl.GetString(gl.VERSION)))
-    fmt.Println(fmt.Sprintf(" -Extensions: %s\n", gl.GetString(gl.EXTENSIONS)))
+func NewWindow(opts WindowOpts) *Window {
+    w := &Window{
+        Title: opts.Title,
+        Width: opts.Width,
+        Height: opts.Height,
+    }
 
-    gl.ClearColor(0.0,0.0,0.0,1.0)
+    initSDL()
 
-    gl.MatrixMode(gl.PROJECTION)
-    gl.LoadIdentity()
+    w.createWindow()
+    w.createContext()
+    w.setVideoOptions()
 
-    gl.Viewport(0, 0, w.Width, w.Height)
-    gl.Ortho(-float64(w.Width)/2, float64(w.Width)/2, -float64(w.Height)/2, float64(w.Height)/2, -1.0, 1.0)
-
-    //Required for per-pixel placing.
-    gl.Translatef (0.375, 0.375, 0.0)
-
-    gl.MatrixMode(gl.MODELVIEW)
-    gl.LoadIdentity()
-
-    gl.Enable(gl.TEXTURE_2D)
-    gl.Disable(gl.LIGHTING)
-    gl.Disable(gl.DITHER)
-
-    gl.Enable(gl.CULL_FACE)
-    gl.CullFace(gl.FRONT)
-
-    gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-
-    gl.Enable(gl.BLEND)
-    gl.Enable(gl.ALPHA_TEST)
-    gl.DepthFunc(gl.LEQUAL)
-
-    gl.Disable(gl.DEPTH_TEST)
+    return w
 }
 
 func (w *Window) Swap() {
@@ -113,3 +49,75 @@ func (w *Window) Destroy() {
     sdl.Quit()
 }
 
+func initSDL() {
+    var err error
+    if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+        panic(err)
+    }
+
+    if err = gl.Init(); err != nil {
+        panic(err)
+    }
+}
+
+func (w *Window) createWindow() {
+    var err error
+    w.Window, err = sdl.CreateWindow(w.Title,
+                                     sdl.WINDOWPOS_UNDEFINED,
+                                     sdl.WINDOWPOS_UNDEFINED,
+                                     int(w.Width), int(w.Height), sdl.WINDOW_OPENGL)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func (w *Window) createContext() {
+    var err error
+    w.Context, err = sdl.GL_CreateContext(w.Window)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func (w *Window) setVideoOptions() {
+    gl.ClearColor(0.0,0.0,0.0,1.0)
+
+    projectionMode()
+    setViewport(w.Width, w.Height)
+
+    //Required for per-pixel placing.
+    gl.Translatef(0.375, 0.375, 0.0)
+
+    modelViewMode()
+    setModelViewOptions()
+}
+
+func projectionMode() {
+    gl.MatrixMode(gl.PROJECTION)
+    gl.LoadIdentity()
+}
+
+func modelViewMode() {
+    gl.MatrixMode(gl.MODELVIEW)
+    gl.LoadIdentity()
+}
+
+func setViewport(width int, height int) {
+    gl.Viewport(0, 0, int32(width), int32(height))
+
+    gl.Ortho(-float64(width)/2, float64(width)/2,
+             -float64(height)/2, float64(height)/2, -1.0, 1.0)
+}
+
+func setModelViewOptions() {
+    gl.Enable(gl.TEXTURE_2D)
+    gl.Disable(gl.LIGHTING)
+    gl.Disable(gl.DITHER)
+    gl.Enable(gl.CULL_FACE)
+    gl.CullFace(gl.FRONT)
+    gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+    gl.Enable(gl.BLEND)
+    gl.Enable(gl.ALPHA_TEST)
+    gl.DepthFunc(gl.LEQUAL)
+    gl.Disable(gl.DEPTH_TEST)
+}
