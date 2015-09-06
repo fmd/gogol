18ddf0e1c8d6bc6fc625dbfaa8e905a0459c0f34
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-06 10:35:18 +0100

    Add vector

---
+++ vector.go
@@ -0,0 +1,15 @@
+package bronson
+
+type Vector struct {
+    X float32
+    Y float32
+}
+
+var (
+    Zero     = Vector{0, 0}
+    Up       = Vector{0, 1}
+    Down     = Vector{0, -1}
+    Left     = Vector{-1, 0}
+    Right    = Vector{1, 0}
+)
+
