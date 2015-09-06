024176b351e967e69224b4b8e39743b9b6b09f30
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-06 10:35:26 +0100

    Add transform

--- transform.go
+++ transform.go
@@ -1,1 +1,11 @@
 package bronson
+
+type Transform struct {
+    parent   *Transform
+    position Vector
+    rotation Vector
+    scale    Vector
+
+    children []*Transform
+    matrix Matrix
+}
