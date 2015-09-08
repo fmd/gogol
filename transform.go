da9addad26f3a77ed4cb2d8842ef1e9f683a5f9f
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-08 22:35:55 +0100

    Add translate and rotate methods

--- transform.go
+++ transform.go
@@ -3,15 +3,34 @@ package gogol
 type Transform struct {
     parent   *Transform
     position Vector
-    rotation Vector
     scale    Vector
-
+    rotation float32
     children []*Transform
     matrix Matrix
+
+    NeedsUpdate bool
 }
 
 func NewTransform(parent *Transform) *Transform {
     return &Transform{
         parent: parent,
+    }
+}
+
+func (t *Transform) Translate(x float32, y float32) {
+    t.position.X = x
+    t.position.Y = y
+    t.SetNeedsUpdate()
+}
+
+func (t *Transform) Rotate(deg float32) {
+    t.rotation += deg
+    t.SetNeedsUpdate()
+}
+
+func (t *Transform) SetNeedsUpdate() {
+    t.NeedsUpdate = true
+    for _, c := range t.children {
+        c.SetNeedsUpdate()
     }
 }
